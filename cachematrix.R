5ce474deb903e01e71fae8146f2d0adaa66f8dea
SrikarMurali <srikarmurali@gmail.com>
2017-06-17 16:21:25 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function allows you to get and set the matrix and its inverse, it lets you access the matrix.
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(x) m <<- x
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks whether the matrix has an inverse already cached, if it does it returns the inverse, if not it calculates the inverse and sets it in the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+
+amatrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+amatrix$get()
+amatrix$getinverse()
+cacheSolve(amatrix)
+cacheSolve(amatrix)
+amatrix$getinverse()
+amatrix$set(matrix(c(2,2,1,4),2,2))
+amatrix$get()
+amatrix$getinverse()
+cacheSolve(amatrix)
+cacheSolve(amatrix)
+amatrix$getinverse()
